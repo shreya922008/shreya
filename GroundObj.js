4d3e272a6638ce935a03289d3fd3182a5982eae2
shreya922008 <72599533+shreya922008@users.noreply.github.com>
2021-03-07 12:13:42 +0530

    Add files via upload

---
+++ GroundObj.js
@@ -0,0 +1,24 @@
+
+class Ground {
+
+    constructor(x, y, width, height) {
+        var options = {
+
+            isStatic: true
+
+        }
+
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+    }
+
+    display() {
+        var pos = this.body.position;
+        strokeWeight(2);
+        fill("black");
+        rect(pos.x, pos.y, this.width, this.height);
+
+    }
+}
